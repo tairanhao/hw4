4162b0053bfc6126a45c56b8e7cde52868edfbcd
tairanhao <55215285+tairanhao@users.noreply.github.com>
2019-10-06 21:03:49 -0700

    Update faster-points.js

--- faster-points.js
+++ faster-points.js
@@ -4,9 +4,10 @@ function setup() {
 }
 
 function draw() {
-  var x = random(width);
-  var y = random(height);
+  for (var x = random(width); x < width; x = x + 100)
+    for (var y = random(height); y < height; y = y + 100) {
 
-  stroke(x, y, 1);
-  point(x, y,1);
+      stroke(x, y, 100);
+      point(x, y);
+    }
 }
